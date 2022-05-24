baaad1778582e478c468a81fa515834b48caccf9
CaoHuynhBaoAn1001 <caoan0012345@gmail.com>
2022-05-24 11:49:55 +0700

    Report GUI

--- QuanLyPhongNet/ReportGUI.Designer.cs
+++ QuanLyPhongNet/ReportGUI.Designer.cs
@@ -28,34 +28,89 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
+            this.components = new System.ComponentModel.Container();
+            Microsoft.Reporting.WinForms.ReportDataSource reportDataSource1 = new Microsoft.Reporting.WinForms.ReportDataSource();
+            this.MemberInformationBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dsMember = new QuanLyPhongNet.dsMember();
+            this.reportViewer1 = new Microsoft.Reporting.WinForms.ReportViewer();
+            this.MemberInformationTableAdapter = new QuanLyPhongNet.dsMemberTableAdapters.MemberInformationTableAdapter();
+            this.dsBill = new QuanLyPhongNet.dsBill();
+            this.AfterPayClientBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.AfterPayClientTableAdapter = new QuanLyPhongNet.dsBillTableAdapters.AfterPayClientTableAdapter();
+            ((System.ComponentModel.ISupportInitialize)(this.MemberInformationBindingSource)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsMember)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsBill)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AfterPayClientBindingSource)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // MemberInformationBindingSource
             // 
-            this.button1.Location = new System.Drawing.Point(112, 122);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(83, 62);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "thoat";
-            this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.ExitClickEventHandler);
+            this.MemberInformationBindingSource.DataMember = "MemberInformation";
+            this.MemberInformationBindingSource.DataSource = this.dsMember;
+            // 
+            // dsMember
+            // 
+            this.dsMember.DataSetName = "dsMember";
+            this.dsMember.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // reportViewer1
+            // 
+            this.reportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            reportDataSource1.Name = "DataSet1";
+            reportDataSource1.Value = this.AfterPayClientBindingSource;
+            this.reportViewer1.LocalReport.DataSources.Add(reportDataSource1);
+            this.reportViewer1.LocalReport.ReportEmbeddedResource = "QuanLyPhongNet.ReportBill.rdlc";
+            this.reportViewer1.Location = new System.Drawing.Point(0, 0);
+            this.reportViewer1.Name = "reportViewer1";
+            this.reportViewer1.ServerReport.BearerToken = null;
+            this.reportViewer1.Size = new System.Drawing.Size(947, 534);
+            this.reportViewer1.TabIndex = 0;
+            // 
+            // MemberInformationTableAdapter
+            // 
+            this.MemberInformationTableAdapter.ClearBeforeFill = true;
+            // 
+            // dsBill
+            // 
+            this.dsBill.DataSetName = "dsBill";
+            this.dsBill.SchemaSerializationMode = System.Data.SchemaSerializationMode.IncludeSchema;
+            // 
+            // AfterPayClientBindingSource
+            // 
+            this.AfterPayClientBindingSource.DataMember = "AfterPayClient";
+            this.AfterPayClientBindingSource.DataSource = this.dsBill;
+            // 
+            // AfterPayClientTableAdapter
+            // 
+            this.AfterPayClientTableAdapter.ClearBeforeFill = true;
             // 
             // ReportGUI
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.button1);
+            this.ClientSize = new System.Drawing.Size(947, 534);
+            this.Controls.Add(this.reportViewer1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "ReportGUI";
             this.Text = "ReportGUI";
             this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.ReportGUI_FormClosed);
+            this.Load += new System.EventHandler(this.ReportGUI_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.MemberInformationBindingSource)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsMember)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dsBill)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.AfterPayClientBindingSource)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private System.Windows.Forms.Button button1;
+        private Microsoft.Reporting.WinForms.ReportViewer reportViewer1;
+        private System.Windows.Forms.BindingSource MemberInformationBindingSource;
+        private dsMember dsMember;
+        private dsMemberTableAdapters.MemberInformationTableAdapter MemberInformationTableAdapter;
+        private System.Windows.Forms.BindingSource AfterPayClientBindingSource;
+        private dsBill dsBill;
+        private dsBillTableAdapters.AfterPayClientTableAdapter AfterPayClientTableAdapter;
     }
 }
